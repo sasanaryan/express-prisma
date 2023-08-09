06a8a822d90c9a71fc96655218b8323d8e2982e3
sasanaryan <sasan4292@gmail.com>
2023-08-09 20:40:58 +0330

    update

--- index.js
+++ index.js
@@ -3,18 +3,20 @@ const cors = require("cors");
 const postRouter = require("./src/routes/post.router");
 const app = express();
 app.use(express.json());
+
 var allowlist = ["http://localhost:8911"];
 var corsOptionsDelegate = function (req, callback) {
   var corsOptions;
   if (allowlist.indexOf(req.header("Origin")) !== -1) {
-    corsOptions = { origin: true }; // reflect (enable) the requested origin in the CORS response
+    corsOptions = { origin: true };
   } else {
-    corsOptions = { origin: false }; // disable CORS for this request
+    corsOptions = { origin: false };
   }
-  callback(null, corsOptions); // callback expects two parameters: error and options
+  callback(null, corsOptions);
 };
 
 app.use(cors(corsOptionsDelegate));
+
 const port = process.env.URL || 8000;
 
 app.use("/posts", postRouter);
